2a3a218d1148195e18d8e8c4b8afff102151046d
penguinarmy94 <luisote94@gmail.com>
2016-11-17 22:18:00 -0800

    added comments

--- app/src/main/java/com/example/triante/translatingheadsetapp/Speaker.java
+++ app/src/main/java/com/example/triante/translatingheadsetapp/Speaker.java
@@ -6,22 +6,24 @@ import android.bluetooth.BluetoothDevice;
  * Created by Jorge Aguiniga on 10/7/2016.
  */
 
+/* Class that handles all the playback and what device should be doing playback*/
 public class Speaker {
-    private double ampLength;
-    private IBMTextToSpeech textToSpeechConverter;
-    private BluetoothDevice earpiece;
-    private BluetoothDevice outSpeaker;
-    private String languageFrom;
-    private String languageTo;
+    private double ampLength; //Max amplitude value of speech from microphone (determine which party is speaking)
+    private IBMTextToSpeech textToSpeechConverter; //Text-to-speech object to use base methods from IBM's Text-to-Speech service
+    private BluetoothDevice earpiece; //placeholder for Bluetooth data from the headset device
+    private BluetoothDevice outSpeaker; //placeholder for Bluetooth data from the external speaker
+    private String languageTo; //language to use for playback
 
     public Speaker (MainActivity instance) {
         textToSpeechConverter = new IBMTextToSpeech(instance);
     }
 
+    /* Method used to find which device should perform playback (Not yet implemented)*/
     public void playback() {
 
     }
 
+    /* Method used to perform playback to a device*/
     public void synthesizeSpeech(String speech) {
         textToSpeechConverter.synthesize(speech, languageTo);
     }
