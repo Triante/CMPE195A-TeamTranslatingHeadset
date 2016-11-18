d78fa2f73a74d9c14d1f74771e17d98c0f86076a
penguinarmy94 <luisote94@gmail.com>
2016-11-17 17:20:20 -0800

    added comments

--- app/src/main/java/com/example/triante/translatingheadsetapp/Authenticator.java
+++ app/src/main/java/com/example/triante/translatingheadsetapp/Authenticator.java
@@ -6,9 +6,15 @@ import java.io.IOException;
  * Created by Jorge Aguiniga on 10/7/2016.
  */
 
+/*Authenticator interface for grabbing token needed to do 
+    translations (MicrosoftTranslate) or speech recognition/synthesis (IBM)
+*/
 public interface Authenticator {
-
+    
+    /*Method creates a placeholder for storing the Authentication token from the cloud service (Microsoft or IBM) */
     public void createAuthToken() throws IOException;
+    /* Method to check whether the current token has expired */
     public boolean isExpired();
+    /* Method to retrieve the token from one of the cloud services (Microsoft or IBM) */
     public String getAuthToken();
 }
