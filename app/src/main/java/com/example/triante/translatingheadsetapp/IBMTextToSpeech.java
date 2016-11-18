8b9a5d92e1b10003d2bb2e2cd6e9ae545d062281
penguinarmy94 <luisote94@gmail.com>
2016-11-17 19:47:47 -0800

    added comments

--- app/src/main/java/com/example/triante/translatingheadsetapp/IBMTextToSpeech.java
+++ app/src/main/java/com/example/triante/translatingheadsetapp/IBMTextToSpeech.java
@@ -11,32 +11,41 @@ import com.ibm.watson.developer_cloud.text_to_speech.v1.model.Voice;
  * Created by Jorge Aguiniga on 10/7/2016.
  */
 
+/* Class to do the text-to-speech conversions when text has been translated from one language to another */
 public class IBMTextToSpeech {
 
-    TextToSpeech textToSpeech;
-    StreamPlayer player = new StreamPlayer();
+    TextToSpeech textToSpeech; //IBM text-to-speech object to access all of its base methods
+    StreamPlayer player = new StreamPlayer(); //Used for playback to an output device
 
 
     public IBMTextToSpeech (Context instance)
     {
-        //Instantiation of TextToSpeech
+        /*URL for IBM's text-to-speech cloud service cloud token factory*/
         String ttsURL = instance.getString(R.string.SpeechSynthesisURLTokenFactory);
+        
+        /* Initialize credentials for accessing the IBM text-to-speech cloud service*/
         String ttsUsername = instance.getString(R.string.SpeechSynthesisUsername);
         String ttsPass = instance.getString(R.string.SpeechSynthesisPassword);
+        
+         /*URL for IBM's text-to-speech cloud service cloud*/
         String ttsServiceURL = "https://stream.watsonplatform.net/text-to-speech/api";
+        
+        /* Begin session*/
         textToSpeech = new TextToSpeech();
         textToSpeech.setUsernameAndPassword(ttsUsername, ttsPass);
     }
-
+    
+    /* Method to convert text input to speech and project it to an audio output device*/
     public void synthesize(String speech, String language) {
         new SynthesizeTask().execute(speech);
     }
 
+    /* Inner class for performing the text-to-speech process as a separate task from the main thread*/
     private class SynthesizeTask extends AsyncTask<String, Void, String> {
 
         @Override protected String doInBackground(String... params) {
-            Voice responseVoice = Language.getResponseLanguageVoice();
-            player.playStream(textToSpeech.synthesize(params[0], responseVoice).execute());
+            Voice responseVoice = Language.getResponseLanguageVoice(); //voice used for playback
+            player.playStream(textToSpeech.synthesize(params[0], responseVoice).execute()); //playback execution
             return "Did syntesize";
         }
     }
