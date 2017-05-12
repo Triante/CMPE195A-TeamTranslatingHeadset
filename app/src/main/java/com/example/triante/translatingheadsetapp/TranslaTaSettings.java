29a7c61745a8f2cb19f99b202904aa1818a561f8
penguinarmy94 <luisote94@gmail.com>
2017-05-11 22:35:43 -0700

    JavaDocs for SettingsActivity and TranslaTaSettings

--- app/src/main/java/com/example/triante/translatingheadsetapp/TranslaTaSettings.java
+++ app/src/main/java/com/example/triante/translatingheadsetapp/TranslaTaSettings.java
@@ -7,16 +7,21 @@ import android.content.SharedPreferences;
  * Created by Jorge Aguiniga on 3/30/2017.
  */
 
+/**
+ * Holds the values for all the TranslaTa settings for the specific user
+ */
 public class TranslaTaSettings {
 
+    /**
+     * Constructor for TranslaTaSettings
+     */
     private TranslaTaSettings() {}
 
-    private static int myLanguageCode = 0;
-    private static int responseLanguageCode = 0;
-    private static boolean profanityFilter = true;
-    private static int maxAmplitude = 0;
-    private static int thresholdAmplitude = 0;
-
+    private static int myLanguageCode = 0; //the language the user chose as his/her speaking language
+    private static int responseLanguageCode = 0; //the language the user chose as the other party's speaking language
+    private static boolean profanityFilter = true; //flag to denote whether the profanity filter is on or not
+    private static int maxAmplitude = 0; //max amplitude possible for system to recognize
+    private static int thresholdAmplitude = 0; //threshold for determining which of the users spoke
     private static final String SHARED_PREFERENCES_FILE_NAME = "TRANSLATA_SHARED_PREFERENCES";
     private static final String SHARED_PREFERENCES_MY_LANGUAGE = "TRANSLATA_MY_LANGUAGE";
     private static final String SHARED_PREFERENCES_RESPONSE_LANGUAGE = "TRANSLATA_RESPONSE_LANGUAGE";
@@ -24,21 +29,41 @@ public class TranslaTaSettings {
     private static final String SHARED_PREFERENCES_MAX_SETTINGS = "TRANSLATA_MAX_SETTINGS";
     private static final String SHARED_PREFERENCES_THRESHOLD_SETTINGS = "TRANSLATA_THRESHOLD_SETTINGS";
 
+    /**
+     * Checks whether the profanity filter is on or not
+     * @return (true if the filter is on, false otherwise)
+     */
     public static boolean isProfanityFilterActive() {
         return profanityFilter;
     }
 
+    /**
+     * Turns on or off the flag for the profanity filter
+     * @param active (true if profanity is meant to be turned on, false otherwise)
+     * @return (status of the profanity filter)
+     */
     public static boolean setProfanityFilter(boolean active) {
         profanityFilter = active;
         return profanityFilter;
     }
 
+    /**
+     * Updates the language that the user chose for himself/herself or other party
+     * @param isMyLanguage (whether it is the user's language or other party's language)
+     * @param code (the language integer code value)
+     * @return (whether the language modification succeeded or not)
+     */
     public static boolean setLanguageSettingsCode(boolean isMyLanguage, int code) {
         if (isMyLanguage) myLanguageCode = code;
         else responseLanguageCode = code;
         return true;
     }
 
+    /**
+     * Sets up the settings variables
+     * @param context (context coming from the settings activity)
+     * @return (whether the settings initiation process succeeded or not)
+     */
     public static boolean initiateTranslaTaSettings(Context context) {
         SharedPreferences settings = context.getSharedPreferences(SHARED_PREFERENCES_FILE_NAME, 0);
         myLanguageCode = settings.getInt(SHARED_PREFERENCES_MY_LANGUAGE, 0);
@@ -53,6 +78,11 @@ public class TranslaTaSettings {
         return true;
     }
 
+    /**
+     * Saves the language settings for both the session and to the phone's local memory
+     * @param context (context coming from the settings activity)
+     * @return (whether the saving process succeeded or not)
+     */
     public static boolean saveLanguageSettings(Context context) {
         SharedPreferences settings = context.getSharedPreferences(SHARED_PREFERENCES_FILE_NAME, 0);
         SharedPreferences.Editor editor = settings.edit();
@@ -63,6 +93,11 @@ public class TranslaTaSettings {
         return true;
     }
 
+    /**
+     * Saves the amplitude threshold preferences for both the session and to the phone's local memory
+     * @param context (context coming from the settings activity)
+     * @return (Whether the saving process succeeded or not)
+     */
     public static boolean saveAmplitudeSettings(Context context) {
         SharedPreferences settings = context.getSharedPreferences(SHARED_PREFERENCES_FILE_NAME, 0);
         SharedPreferences.Editor editor = settings.edit();
@@ -72,18 +107,34 @@ public class TranslaTaSettings {
         return true;
     }
 
+    /**
+     * Accessor for the max amplitude possible
+     * @return (the max amplitude value)
+     */
     public static int getMaxAmplitude() {
         return maxAmplitude;
     }
 
+    /**
+     * Accessor for the current amplitude threshold
+     * @return (the amplitude threshold)
+     */
     public static int getThresholdAmplitude() {
         return thresholdAmplitude;
     }
 
+    /**
+     * Mutator for the max amplitude
+     * @param maxAmp (max amplitude value)
+     */
     public static void setMaxAmplitude(int maxAmp) {
         maxAmplitude = maxAmp;
     }
 
+    /**
+     * Mutator for the amplitude threshold
+     * @param thresholdAmp (amplitude threshold value)
+     */
     public static void setAmplitudeThreshold(int thresholdAmp) {
         thresholdAmplitude = thresholdAmp;
     }
